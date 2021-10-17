6fbcada36da618813bed47cd38c56aab49422855
Joy Mondal <84225212+codewithjoymondal@users.noreply.github.com>
2021-10-17 16:38:58 +0100

    Add files via upload

---
+++ Auto Sms Sender.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+while True:
+    time.sleep(2)
+    pyautogui.typewrite('Here Your text As like Subscribe Codewithjoy Youtube Channel')
+    pyautogui.press('enter')
+
